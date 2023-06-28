a75385bd48799ff872dd63c3dd0d7ae1502b56c3
alonc7 <kochmanstudent@gmail.com>
2023-06-28 22:12:58 +0300

    moved index.js from DB client to DB server

---
+++ DB/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+
+app.use(express.json());
+
+const MongoClient = require('mongodb').MongoClient;
+
+const uri = 'mongodb://localhost:27017/TimeLoop'; //  MongoDB connection and database name
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+client.connect(err => {
+  if (err) {
+    console.log('Error connecting to the database:', err);
+    return;
+  }
+
+  console.log('Connected to the database');
+
+  // Add your API routes and logic here
+
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
